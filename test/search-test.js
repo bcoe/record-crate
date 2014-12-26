3412bac854851d4b599a1dd4889c63390bd1a59e
bcoe <bencoe@gmail.com>
2014-12-26 13:42:41 -0600

    added more tests, simplified sorting DSL.

--- test/search-test.js
+++ test/search-test.js
@@ -10,7 +10,7 @@ lab.experiment('Search', function () {
   lab.it('should return the first page of search results if no pagination given', function(done) {
     var defaultSearch = nock('http://localhost:9200')
       .post('/record-crate/song/_search?size=100&from=0&q=*', {
-        "sort": [{bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
+        "sort": [{_score: {order: "desc"}}, {bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
       })
       .reply(
         200,
@@ -18,10 +18,12 @@ lab.experiment('Search', function () {
       ),
       search = new Search();
 
-    search.search(0, [], '*', function(err, obj) {
+    search.search(0, null, '*', function(err, obj) {
       Code.expect(obj[0].title).to.equal('Enter The Seed (Original Mix)');
       Code.expect(obj[0].artist).to.equal('Plantrae');
       Code.expect(obj.total).to.equal(317);
+      Code.expect(obj.page).to.equal(0);
+      Code.expect(obj.page_size).to.equal(100);
       return done();
     });
   });
@@ -29,7 +31,7 @@ lab.experiment('Search', function () {
   lab.it('replaces missing bpm with n/a', function(done) {
     var defaultSearch = nock('http://localhost:9200')
       .post('/record-crate/song/_search?size=100&from=0&q=*', {
-        "sort": [{bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
+        "sort": [{_score: {order: "desc"}}, {bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
       })
       .reply(
         200,
@@ -37,7 +39,7 @@ lab.experiment('Search', function () {
       ),
       search = new Search();
 
-    search.search(0, [], '*', function(err, obj) {
+    search.search(0, null, '*', function(err, obj) {
       Code.expect(obj[0].bpm).to.equal('n/a');
       return done();
     });
@@ -46,7 +48,7 @@ lab.experiment('Search', function () {
   lab.it('replaces missing artist with unknown', function(done) {
     var defaultSearch = nock('http://localhost:9200')
       .post('/record-crate/song/_search?size=100&from=0&q=*', {
-        "sort": [{bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
+        "sort": [{_score: {order: "desc"}}, {bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
       })
       .reply(
         200,
@@ -54,9 +56,42 @@ lab.experiment('Search', function () {
       ),
       search = new Search();
 
-    search.search(0, [], '*', function(err, obj) {
+    search.search(0, null, '*', function(err, obj) {
       Code.expect(obj[1].artist).to.equal('unknown');
       return done();
     });
+  });
+
+  lab.it('allows page number to be modified', function(done) {
+    var defaultSearch = nock('http://localhost:9200')
+      .post('/record-crate/song/_search?size=100&from=100&q=*', {
+        "sort": [{_score: {order: "desc"}}, {bpm: {order: "desc"}}, {sortable_title: {order: "asc"}}]
+      })
+      .reply(
+        200,
+        JSON.parse(fs.readFileSync('./test/fixtures/search/star-p0.json', 'utf-8'))
+      ),
+      search = new Search();
+
+    search.search(1, null, '*', function(err, obj) {
+      Code.expect(obj.page).to.equal(1);
+      Code.expect(obj.page_size).to.equal(100);
+      Code.expect(obj.total).to.equal(317);
+      return done();
+    });
+  });
+
+  lab.it('allows sort order to be modified', function(done) {
+    var defaultSearch = nock('http://localhost:9200')
+    .post('/record-crate/song/_search?size=100&from=0&q=*', {
+      "sort": [{_score: {order: "desc"}}, {artist: {order: "asc"}}]
+    })
+    .reply(
+      200,
+      JSON.parse(fs.readFileSync('./test/fixtures/search/star-p0.json', 'utf-8'))
+    ),
+    search = new Search();
+
+    search.search(0, [['artist', 'asc']], '*', done);
   });
 });
